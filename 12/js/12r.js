de1c185a3e0cd5a7cddcb7f79ce0314f9cb912c3
Manu199 <Manu199gh@gmail.com>
2025-09-16 16:30:44 +0200

    [add] all 12 exercises

---
+++ 12/js/12r.js
@@ -0,0 +1,167 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    loses: 0,
+    ties: 0,
+}
+
+document.querySelector('.js-score')
+.innerHTML = `Wins: ${score.wins}, Loses: ${score.loses}, Ties: ${score.ties}`;
+
+/* if (!score){
+    score = {
+        wins: 0,
+        loses: 0,
+        ties: 0,
+    }
+}
+*/
+
+let isAutoPlaying = false;
+let intervalId;
+
+// const autoPlay = () => {
+
+// };
+
+const autoPlayButton = document.querySelector('.auto-play-btn').addEventListener('click', autoPlay = () =>
+    
+    
+    {if(!isAutoPlaying){
+        intervalId = setInterval(() => {
+            const playerMove = pickComputerMove();
+            playGame(playerMove); 
+        },1000);
+        isAutoPlaying = true;
+        document.querySelector('.auto-play-btn').innerHTML = 'Stop Playing'
+    }else {
+            clearInterval(intervalId);
+            isAutoPlaying = false;
+            document.querySelector('.auto-play-btn').innerHTML = 'AutoPlay'
+
+        }
+})
+
+
+document.querySelector('.js-rock-btn')
+.addEventListener('click', () => {
+    playGame('rock');
+})
+
+document.querySelector('.js-paper-btn')
+.addEventListener('click', () => {
+    playGame('paper');
+})
+
+document.querySelector('.js-scissors-btn')
+.addEventListener('click', () => {
+    playGame('scissors');
+})
+
+document.body.addEventListener('keydown', (event) => {
+    if(event.key === 'r'){
+        playGame('rock');
+    }else if(event.key === 'p'){
+        playGame('paper');
+    }else if(event.key === 's'){
+        playGame('scissors');
+    }else if(event.key === 'a'){
+        autoPlay();
+    }else if(event.key === 'Backspace'){
+        resetScore();
+    }
+});
+
+function playGame(playerMove) {
+
+    const computerMove = pickComputerMove();      
+    let result = '';       
+    
+    if (playerMove === 'scissors') {
+        if (computerMove === 'rock') {
+        result = 'You lose.';
+        } else if (computerMove === 'paper') {
+        result = 'You win!';
+        }else if (computerMove === 'scissors') {
+        result = 'Tie.';
+        }
+
+    }else if (playerMove === 'paper'){
+        if (computerMove === 'rock') {
+        result = 'You win!';
+        } else if (computerMove === 'paper') {
+        result = 'Tie.';
+        }else if (computerMove === 'scissors') {
+        result = 'You lose.';
+        }
+
+    }else if (playerMove === 'rock'){
+        if (computerMove === 'rock') {
+        result = 'Tie.';
+        } else if (computerMove === 'paper') {
+        result = 'You lose.';
+        }else if (computerMove === 'scissors') {
+        result = 'You win!';
+        }
+    }
+
+    if (result === 'You win!'){
+        score.wins += 1;
+    } else if (result === 'You lose.'){
+        score.loses += 1;
+    } else if (result === 'Tie.'){
+        score.ties += 1;
+    }
+
+    localStorage.setItem('score', JSON.stringify(score));
+
+    
+    document.querySelector('.js-result')
+        .innerHTML = result;
+    
+    document.querySelector('.js-moves')
+        .innerHTML = `You <img src="images/${playerMove}-emoji.png" class="move-icon"> <img src="images/${computerMove}-emoji.png" class="move-icon"> Computer`;
+
+    updateScoreElement();
+
+}
+
+function updateScoreElement() {
+document.querySelector('.js-score')
+    .innerHTML = `Wins: ${score.wins}, Loses: ${score.loses}, Ties: ${score.ties}`;
+
+}
+
+
+function pickComputerMove() {
+
+    const randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3){
+        computerMove = ('rock');
+    }else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3){
+        computerMove = ('paper');
+    }else if (randomNumber >= 2 /3 && randomNumber < 1){
+        computerMove = ('scissors');
+    }
+
+    return computerMove;
+}
+
+const message = document.querySelector('.div-message')
+const yesReset = document.querySelector('.yes-btn')
+const noReset = document.querySelector('.no-btn')
+
+const resetScoreBtn = document.querySelector('.reset-score-btn').addEventListener('click', resetScore = () => {
+        message.classList.add('show')
+        yesReset.addEventListener('click', () =>{
+        score.loses = 0;
+        score.wins = 0;
+        score.ties = 0;
+        localStorage.removeItem('score');
+        updateScoreElement();
+        message.classList.remove('show')})
+        noReset.addEventListener('click', () => { message.classList.remove('show') })
+})
+
