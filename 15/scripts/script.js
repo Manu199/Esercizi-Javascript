b8ea6ba68415ac52ca9e34b444336ec397da140f
Manu199 <Manu199gh@gmail.com>
2025-09-30 13:38:04 +0200

    exercise 15b

--- 15/scripts/script.js
+++ 15/scripts/script.js
@@ -6,3 +6,8 @@ const monthDay = dayjs();
 const futureDate = monthDay.add(7, 'day');
 
 console.log(futureDate.format('MM-DD'));
+
+
+/* exercise 15b */
+const futureMonth = monthDay.add(1, 'month');
+console.log(futureMonth.format('MM-DD'));
