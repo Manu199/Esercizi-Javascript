daee9e4b17ed58203bedb952abcf8fec7f439a98
Manu199 <Manu199gh@gmail.com>
2025-09-30 13:35:22 +0200

    exercise 15a

---
+++ 15/scripts/script.js
@@ -0,0 +1,8 @@
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+
+
+/* exercise 15a */
+const monthDay = dayjs();
+const futureDate = monthDay.add(7, 'day');
+
+console.log(futureDate.format('DD-MM-YYYY'));
